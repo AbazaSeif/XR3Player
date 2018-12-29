32b38170dfa68d73ca8190a94e9c2b3371808e6a
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-12-29 16:05:41 +0200

    Modularizing project

---
+++ src/module-info.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+/**
+ * @author GOXR3PLUSSTUDIO
+ *
+ */
+module XR3Player {
+	exports main.java.com.goxr3plus.xr3player.controllers.loginmode;
+	exports main.java.com.goxr3plus.xr3player.xr3capture;
+	exports main.java.com.goxr3plus.xr3player.application;
+	exports main.java.com.goxr3plus.xr3player.models.smartcontroller;
+	exports main.java.com.goxr3plus.xr3player.application.database;
+	exports main.java.com.goxr3plus.xr3player.services.smartcontroller;
+	exports main.java.com.goxr3plus.xr3player.services.zipping;
+	exports main.java.com.goxr3plus.xr3player.controllers.djmode;
+	exports main.java.com.goxr3plus.xr3player.application.xplayer.waveform;
+	exports main.java.com.goxr3plus.xr3player.controllers.windows;
+	exports main.java.com.goxr3plus.xr3player.controllers.chromium;
+	exports main.java.com.goxr3plus.xr3player.controllers.general;
+	exports main.java.com.goxr3plus.xr3player.utils.io;
+	exports main.java.com.goxr3plus.xr3player.controllers.custom;
+	exports main.java.com.goxr3plus.xr3player.controllers.librarymode;
+	exports main.java.com.goxr3plus.xr3player.services.chromium;
+	exports main.java.com.goxr3plus.xr3player.services.dropbox;
+	exports main.java.com.goxr3plus.xr3player.services.xplayer;
+	exports main.java.com.goxr3plus.xr3player.utils.general;
+	exports main.java.com.goxr3plus.xr3player.controllers.tagging;
+	exports main.java.com.goxr3plus.xr3player.utils.javafx;
+	exports main.java.com.goxr3plus.xr3player.application.xplayer.visualizer.core;
+	exports main.java.com.goxr3plus.xr3player.services.systemtree;
+	exports main.java.com.goxr3plus.xr3player.controllers.systemtree;
+	exports main.java.com.goxr3plus.xr3player.services.loginmode;
+	exports main.java.com.goxr3plus.xr3player.controllers.smartcontroller;
+	exports main.java.com.goxr3plus.xr3player.controllers.xplayer;
+	exports main.java.com.goxr3plus.xr3player.application.xplayer.visualizer.presenter;
+	exports main.java.com.goxr3plus.xr3player.application.xplayer.microphone;
+	exports main.java.com.goxr3plus.xr3player.application.enums;
+	exports main.java.com.goxr3plus.xr3player.application.xplayer.dsp;
+	exports main.java.com.goxr3plus.xr3player.services.general;
+	exports main.java.com.goxr3plus.xr3player.models.xplayer;
+	exports main.java.com.goxr3plus.xr3player.application.xplayer.visualizer.geometry;
+	exports main.java.com.goxr3plus.xr3player.models.lists;
+	exports main.java.com.goxr3plus.xr3player.services.database;
+	exports main.java.com.goxr3plus.xr3player.xr3capture.model;
+	exports main.java.com.goxr3plus.xr3player.controllers.dropbox;
+	exports main.java.com.goxr3plus.xr3player.controllers.moviemode;
+	exports main.java.com.goxr3plus.xr3player.controllers.settings;
+	
+//	requires FX.BorderlessScene.V3.1.0;
+	requires com.jfoenix;
+	requires commons.validator;
+	requires controlsfx;
+	requires dropbox.core.sdk;
+	requires evo.inflector;
+	requires flowless;
+//	requires image4j;requires jaudiotagger.V2.2.6;
+	requires java.desktop;
+	requires java.flac.encoder;
+//	requires java.google.speech.api.V2.1;
+	requires java.logging;
+	requires java.sql;
+//	requires java.stream.player.V8.0.0;
+	requires javasysmon2;
+	requires jave.core;
+	requires jnativehook;
+	requires json;
+	requires jxbrowser;
+	requires mp3agic;
+	requires org.apache.commons.io;
+	requires org.jsoup;
+	requires org.kordamp.ikonli.javafx;
+	requires richtextfx;
+	requires zt.zip;
+	requires javafx.graphics;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.swing;
+	requires javafx.media;
+	}
